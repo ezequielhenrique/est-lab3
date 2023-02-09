fbfa540dc2c46cca5a0638b4682ff493a2a43a03
Ezequiel Henrique <ezequielhenrique806@gmail.com>
2023-02-09 17:59:26 +0000

    Minimum and maximum

--- java/ListaLigada.java
+++ java/ListaLigada.java
@@ -1,99 +1,6 @@
 public class ListaLigada implements EstruturaDeDados{
     private No inicio;
 
-    public void removeInicio (){
-        if (inicio != null)
-            inicio = inicio.getProximo();
-    }
-
-    public void removeFim (){
-        if (inicio == null){
-            return;
-        }
-        if (inicio.getProximo() == null){
-            inicio = null;
-        }
-        removeFim(inicio);
-    }
-
-    public void removeFim (No n){
-        No proximo = n.getProximo();
-        if (proximo.getProximo() == null){
-            n.setProximo(null);
-            return;
-        } else{
-            removeFim(proximo);
-        }
-    }
-
-    public void insereInicio (int valor){
-        if(inicio == null){
-            inicio = new No(valor);
-            return;
-        }
-        No n = new No(valor);
-        n.setProximo(inicio);
-        inicio = n;
-
-    }
-
-    public void insereFim (int valor){
-        if(inicio == null){
-            inicio = new No(valor);
-            return;
-        }
-        insere(inicio, valor);
-    }
-
-    public boolean procura (int valor){
-        if (inicio == null){
-            return false;
-        } else {
-            return procura(inicio, valor);
-        }
-    }
-
-    public boolean procura (No n, int valor){
-        if (n.getValor() == valor){
-            return true;
-        } else if (n.getProximo() == null){
-            return false;
-        } else {
-            return procura(n.getProximo(), valor);
-        }
-    }
-    public void insere (No n, int valor){
-        if (n.getProximo() == null){
-            No novo = new No(valor);
-            n.setProximo(novo);
-        } else {
-            insere(n.getProximo(), valor);
-        }
-    }
-
-    public void remover(int valor){
-        if (inicio == null){
-            return;
-        }
-        if (inicio.getValor() == valor){
-            inicio = inicio.getProximo();
-            return;
-        }
-        remover(inicio, valor);
-    }
-
-    public void remover(No n, int valor){
-        No proximo = n.getProximo();
-        if (proximo == null){
-            return;
-        }
-        if (proximo.getValor() == valor){
-            n.setProximo(proximo.getProximo());
-        } else{
-            remover(proximo, valor);
-        }
-    }
-
     public static void main(String[] args) {
         ListaLigada lista = new ListaLigada();
 
@@ -106,15 +13,15 @@ public class ListaLigada implements EstruturaDeDados{
         System.out.println(lista.search(5));
         System.out.println(lista.search(7));
         System.out.println(lista.search(11));
-        System.out.println(lista.search(20));
+
+        System.out.println(lista.minimum());
+        System.out.println(lista.maximum());
+
         System.out.println(lista.delete(10));
         System.out.println(lista.search(10));
         System.out.println(lista.delete(9));
         System.out.println(lista.search(9));
         System.out.println(lista.delete(1));
-        System.out.println(lista.delete(5));
-        System.out.println(lista.search(5));
-
     }
 
     @Override
@@ -185,14 +92,60 @@ public class ListaLigada implements EstruturaDeDados{
 
     @Override
     public int minimum() {
-        // TODO Auto-generated method stub
-        return 0;
+        if (this.inicio == null) {
+            return -1;
+        } else {
+            int minimo = this.inicio.getValor();
+
+            if (inicio.getProximo() == null) {
+                return minimo;
+            } else {
+                return comparaMinimo(this.inicio, minimo);
+            }
+        }
+    }
+
+    public int comparaMinimo(No node, int minimo) {
+        int menor = minimo;
+
+        if (node.getProximo() == null) {
+            return minimo;
+        }
+        
+        if (node.getProximo().getValor() < minimo) {
+            menor = node.getProximo().getValor();
+        } 
+
+        return comparaMinimo(node.getProximo(), menor);
     }
 
     @Override
     public int maximum() {
-        // TODO Auto-generated method stub
-        return 0;
+        if (this.inicio == null) {
+            return -1;
+        } else {
+            int maximo = this.inicio.getValor();
+
+            if (inicio.getProximo() == null) {
+                return maximo;
+            } else {
+                return comparaMaximo(this.inicio, maximo);
+            }
+        }
+    }
+
+    public int comparaMaximo(No node, int maximo) {
+        int maior = maximo;
+
+        if (node.getProximo() == null) {
+            return maximo;
+        }
+        
+        if (node.getProximo().getValor() > maximo) {
+            maior = node.getProximo().getValor();
+        } 
+
+        return comparaMaximo(node.getProximo(), maior);
     }
 
     @Override
